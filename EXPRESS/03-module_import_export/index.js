12f3ea5d8c95de6ff737d567872dbfda1c8637af
Veer <bveee369@gmial.com>
2025-08-29 07:29:08 +0530

    using html files

--- EXPRESS/03-module_import_export/index.js
+++ EXPRESS/03-module_import_export/index.js
@@ -1,5 +1,9 @@
-// import express from "express"
-const express = require("express")
+import express from "express"
+// const express = require("express")
+// const { default: about } = require("./Modules/about.js")
+// const home= require("./Modules/home.js")
+import home from "./Modules/home.js"
+import about from "./Modules/about.js"
 
 
 const app = express()
@@ -10,12 +14,17 @@ const api = [{ id: 1, name: "veer", course: "MERN" },
 { id: 2, name: "veer" }, { id: 3, name: "veer" },
 { id: 4, name: "veer" }, { id: 5, name: "veer" }
 ]
-if (path) {
+
     app.get("/", (req, res) => {
-        res.send(api)
+        res.send(home())
+        // console.log("api works")
     })
-}
+
+app.get("/about",(req,res)=>{
+    res.send(about())
+    console.log("about is working")
+})
 
 app.listen(4000, () => {
     console.log("port running on 4000")
-})
+}) 
